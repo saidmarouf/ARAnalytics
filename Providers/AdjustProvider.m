4f38de603c423986d54a99814ef730bbb8c6e66f
Said M. Marouf <marouf.said@gmail.com>
2015-01-30 12:04:10 +0100

    Update AdjustProvider.m
    Adjust 4.0

--- Providers/AdjustProvider.m
+++ Providers/AdjustProvider.m
@@ -1,29 +1,24 @@
-//
-//  AdjustProvider.m
-//
-//  Created by Engin Kurutepe on 10/11/2014.
-//  Copyright (c) 2014 Engin Kurutepe. All rights reserved.
-//
-
 #import "AdjustProvider.h"
 #import "ARAnalyticsProviders.h"
 #import "Adjust.h"
 
 @interface AdjustProvider()
 @property (nonatomic, strong) NSDictionary *eventTokenMap;
+@property (nonatomic, weak) id adjustDelegate;
 @end
 
 
 @implementation AdjustProvider
 
+static ADJConfig *sharedAdjustConfigurator = nil;
+
 - (id)initWithIdentifier:(NSString *)identifier eventTokenMap:(NSDictionary *)eventTokenMap {
     NSAssert([Adjust class], @"Adjust is not included");
-    [Adjust appDidLaunch:identifier];
     
 #if defined (DEBUG)
-    [Adjust setEnvironment:AIEnvironmentSandbox];
+    sharedAdjustConfigurator = [ADJConfig configWithAppToken:identifier environment:ADJEnvironmentSandbox];
 #else
-    [Adjust setEnvironment:AIEnvironmentProduction];
+    sharedAdjustConfigurator = [ADJConfig configWithAppToken:identifier environment:ADJEnvironmentProduction];
 #endif
     
     if(self = [super init]) {
@@ -34,8 +29,24 @@
 }
 
 - (void)event:(NSString *)event withProperties:(NSDictionary *)properties {
-    if(_eventTokenMap && _eventTokenMap[event])
-        [Adjust trackEvent:_eventTokenMap[event] withParameters:properties];
+    if(_eventTokenMap && _eventTokenMap[event]) {
+        ADJEvent *e = [ADJEvent eventWithEventToken:_eventTokenMap[event]];
+        if(properties) {
+            for (NSString *pKey in properties) {
+                if(properties[pKey] && properties[pKey] != [NSNull null])
+                    [e addCallbackParameter:pKey value:properties[pKey]];
+            }
+        }
+        [Adjust trackEvent:e];
+    }
+}
+
++ (void) setAdjustDelegate:(id<AdjustDelegate>) delegate {
+    [sharedAdjustConfigurator setDelegate:delegate];
+}
+
++ (void) appDidLaunch {
+    [Adjust appDidLaunch:sharedAdjustConfigurator];
 }
 
 @end
